2cda54dc55ebd6189137274af028f59369e0ffa3
Matthew-Shelby <matteo.maloni@student.univaq.it>
2025-10-19 13:08:51 +0200

    aggiunto profilo

---
+++ view/Vprofilo.js
@@ -0,0 +1,11 @@
+export default class VProfilo {
+
+    constructor() {
+        this.router = null; // Router instance
+        this.controller = null; // Controller instance
+    }
+
+    init() {
+        console.log("VProfilo initialized");
+    }
+}
