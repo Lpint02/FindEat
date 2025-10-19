0587a9e61e8a81611eae9cc7f29dcb532fb19bb2
Matthew-Shelby <matteo.maloni@student.univaq.it>
2025-10-19 22:38:24 +0200

    aggiunte nuove schermate di home e registrazione e collegate tra loro. Logout fatto , login pure, e tutta compagnia bella.  Nuovo record  9h di programmazione di fila! Mi sono leggermente rotto il C**** . Detto ciò vado a cenare se mamma non mi ha chiuso fuori di casa. Buonanotte ;-)

--- view/LoginView.js
+++ view/LoginView.js
@@ -13,19 +13,32 @@ export default class LoginView
    * Metodo chiamato dal Router dopo che l'HTML è stato caricato
    */
   init() {
+    console.log("LoginView initialized");
     // Riferimenti agli elementi DOM già presenti nella pagina
-    this.form = document.getElementById("loginForm");
-    this.username = document.getElementById("username");
-    this.password = document.getElementById("password");
-    this.submitButton = document.getElementById("loginButton");
+    //this.form = document.getElementById("loginForm");
+    //this.username = document.getElementById("username");
+    //this.password = document.getElementById("password");
+    //this.submitButton = document.getElementById("loginButton");
     
 
     // Collego gli eventi al presenter
-    this.submitButton.addEventListener("click", (e) => {
-      e.preventDefault(); // previene il comportamento di default del form
-      const email = document.getElementById("username").value;
-      const password = document.getElementById("password").value;
-      this.controller.handleLogin(email, password);
+    //this.submitButton.addEventListener("click", (e) => {
+      //e.preventDefault(); // previene il comportamento di default del form
+      //const email = document.getElementById("username").value;
+      //const password = document.getElementById("password").value;
+      //this.controller.handleLogin(email, password);
+    //});
+
+    
+    document.querySelector('form').addEventListener('submit', (e) => {
+      e.preventDefault();
+      const email = document.getElementById('typeEmailX').value;
+      const password = document.getElementById('typePasswordX').value;
+      if (this.controller && typeof this.controller.handleLogin === 'function') {
+        this.controller.handleLogin(email, password);
+      } else {
+        console.error('controller.handleLogin non definito');
+      }
     });
   }
 
