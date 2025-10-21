a977f42c6ff7182ada36df90d9f6b9055e28f177
Nadia Muzyka <muzykanadia0@gmail.com>
2025-10-21 10:46:38 +0200

    fix registation

--- view/RegistrationView.js
+++ view/RegistrationView.js
@@ -19,7 +19,12 @@ export default class RegistrationView {
       const password = document.getElementById('regPassword').value;
       const password2 = document.getElementById('regPassword2').value;
 
-      if (this.controller && typeof this.controller.handleRegistration === 'function' && password === password2) {
+      if (password !== password2) {
+        allert("Le password non coincidono");
+        return;
+      }
+
+      if (this.controller && typeof this.controller.handleRegistration === 'function') {
         this.controller.handleRegistration(email, password, name);
       } else {
         console.error('controller.handleRegistration non definito');
